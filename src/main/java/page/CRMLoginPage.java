3f46770ee8db1160ba6b8ff5f823fa6fb827ac75
Sumeetra <59745288+Sumeetra@users.noreply.github.com>
2020-05-29 18:19:54 -0500

    first commit

---
+++ src/main/java/page/CRMLoginPage.java
@@ -0,0 +1,38 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class CRMLoginPage extends CRMBasePage {
+
+	WebDriver driver;
+
+	// Every Page must have a constructor to invite the driver
+	public CRMLoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	// Element Library
+	@FindBy(how = How.XPATH, using = "//a[@class='btn btn-primary btn-xs-2 btn-shadow btn-rect btn-icon btn-icon-left']/child::span[text() = 'Log In' ]")
+	WebElement LogInButton;
+	@FindBy(how = How.NAME, using = "email")
+	WebElement UserEmail;
+	@FindBy(how = How.NAME, using = "password")
+	WebElement Password;
+	@FindBy(how = How.XPATH, using = "//div[@class='ui fluid large blue submit button']")
+	WebElement SignInButton;
+
+	// Methods to interact with the elements
+	public void login(String userEmail, String passWord) {
+		LogInButton.click();
+		UserEmail.sendKeys(userEmail);
+		Password.sendKeys(passWord);
+		SignInButton.click();
+	}
+
+	public String getPageTitle() {
+		return driver.getTitle();
+	}
+}
---
+++ src/test/java/test/CRMTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import page.CRMBasePage;
+import page.CRMLoginPage;
+import util.CRMBrowserFactory;
+
+public class CRMTest extends CRMBasePage {
+
+	WebDriver driver;
+
+	@Test(priority = 1)
+	public void createQuickDraft() throws InterruptedException {
+		driver = CRMBrowserFactory.startBrowser();
+
+		CRMLoginPage loginPage = PageFactory.initElements(driver, CRMLoginPage.class);
+
+		loginPage.login("automationtestquality@gmail.com", "Automation12");
+	}
+	}
